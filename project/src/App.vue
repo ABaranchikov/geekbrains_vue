871922fdab4d462b5271dce94b33cc590316660c
unknown <ryoken@mail.ru>
2021-07-13 11:42:18 +0300

    lesson04 - router

--- project/src/App.vue
+++ project/src/App.vue
@@ -2,7 +2,27 @@
   <div id="app" :class="[$style.wrapper]">
     <header>
       <h1>My personal cost</h1>
+      <div class="menu">
+        <router-link to="/dashboard">Dashboard</router-link> /
+        <router-link to="/about">About</router-link>/
+        <!--
+        <router-link to="/notfound">NotFound</router-link>
+-->
+        <button @click="goToPageNotFound">Not found</button>
+        <!--
+        <a href="dashboard">Dashboard</a> / <a href="about">About</a> /
+        <a href="notfound">Not Found</a>
+        -->
+      </div>
       <main>
+        <div class="content-page">
+          <router-view />
+          <!--
+          <About v-if="page === 'about'" />
+          <Dashboard v-if="page === 'dashboard'" />
+          <NotFound v-if="page === 'notfound'" />
+          -->
+        </div>
         <div>
           <AddPayment @addNewPayment="addData" :categories="categories" />
         </div>
@@ -30,7 +50,11 @@ import CategorySelect from "./components/CategorySelect.vue";
 
 import { mapGetters, mapMutations, mapActions } from "vuex";
 import Pagination from "./components/Pagination.vue";
-
+/*
+import About from "./views/About.vue";
+import NotFound from "./views/NotFound.vue";
+import Dashboard from "./views/Dashboard.vue";
+*/
 export default {
   name: "App",
   components: {
@@ -38,8 +62,15 @@ export default {
     AddPayment,
     Pagination,
     CategorySelect,
+    //  About,
+    //  NotFound,
+    // Dashboard,
   },
-
+  data() {
+    return {
+      page: "",
+    };
+  },
   methods: {
     ...mapMutations(["setPaymentListData", "addDataToPaymentsList"]),
     ...mapActions(["fetchData", "fetchCategory", "addCategoryToList"]),
@@ -55,6 +86,19 @@ export default {
     changePage(page) {
       console.log("Page = " + page);
       this.fetchData(page);
+    },
+    /*
+    setMenuPage() {
+      //навигация через hash
+      //this.page = location.hash.slice(1);
+      this.page = location.pathname.slice(1);
+    },
+    
+    */
+    goToPageNotFound() {
+      this.$router.push({
+        name: "NotFound",
+      });
     },
   },
 
@@ -82,6 +126,29 @@ export default {
     if (!this.categories.length) {
       this.fetchCategory();
     }
+  },
+
+  mounted() {
+    /*
+    //навигация через hash
+    this.setMenuPage();
+    window.addEventListener("hashchange", () => {
+      this.setMenuPage();
+    });
+    */
+    /*
+   //через историю
+    this.setMenuPage();
+    const links = document.querySelectorAll("a");
+    links.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        history.pushState({}, "", link.href);
+        this.setMenuPage();
+      });
+    });
+    window.addEventListener("popstate", this.setMenuPage);
+    */
   },
 };
 </script>
---
+++ project/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Dashboard from '../views/Dashboard'
+import About from '../views/About'
+import NotFound from '../views/NotFound'
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/dashboard',
+            component: Dashboard,
+            name: 'dashboard'
+        },
+        {
+            path: '/',
+            component: Dashboard,
+            name: 'dashboard'
+        },
+        {
+            path: '/about',
+            component: About,
+            name: 'about'
+        },
+        //  {
+        //      path: '/notfound',
+        //     component: NotFound,
+        //      name: 'NotFound'
+        //   },
+        {
+            path: '*',
+            component: NotFound,
+        }
+    ],
+
+})
